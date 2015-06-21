69de2ba00defd368f360387334c1b376db79f9db
German Martinez <germaaan@gmail.com>
2015-06-22 01:49:18 +0200

    Añadidos los dos primeros puntos de implementación

--- makePDF.sh
+++ makePDF.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #Generación de PDF compilando el archivo LaTeX con pdflatex
-cd text && pdflatex -synctex=1 -interaction=nonstopmode -output-directory=../ proyecto.tex
+cd text
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=../ proyecto.tex
+pdflatex -synctex=1 -interaction=nonstopmode -output-directory=../ proyecto.tex
+cd ..
+rm {*.aux,*.lof,*.log,*.lol,*.lot,*.out,*.synctex.gz,*toc}
