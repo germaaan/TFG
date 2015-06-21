72f5633fa8876989813984aaa00f0d864dded02b
German Martinez <germaaan@gmail.com>
2015-06-21 16:09:28 +0200

    Añadida comprobación ortográfica mediante integración continua. Ref #1.

--- makePDF.sh
+++ makePDF.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Generación de PDF compilando el archivo LaTeX con pdflatex
-pdflatex -synctex=1 -interaction=nonstopmode proyecto.tex
+cd text && pdflatex -synctex=1 -interaction=nonstopmode -output-directory=../ proyecto.tex
