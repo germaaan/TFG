bb24d5d6f5ac48a7c8bfe59867fb42f0f3a12460
German Martinez <germaaan@gmail.com>
2015-07-05 12:42:56 +0200

    Soluciona ordenación de barras de gráficos. Fix #12

--- graphics/data/solicitudesSegundo.R
+++ graphics/data/solicitudesSegundo.R
@@ -1,12 +1,19 @@
 #!/usr/bin/env Rscript
 library("ggplot2")
 
-datos <- data.frame(solicitudes = factor(c("30","30","30","50","50","50","100","100","100")),
-                    concurrencia = factor(c("25%","50%","75%","25%","50%","75%","25%","50%","75%"),
-                                          levels = c("25%","50%","75%")),
-                    solsSegundo = c(3.402,3.976,4.470,4.898,4.000,3.583,3.728,4.496,4.197))
+solicitudes <- c(30,30,30,50,50,50,100,100,100)
+concurrencia <- c("25%","50%","75%","25%","50%","75%","25%","50%","75%")
+solicitudesSegundo <- c(3.402,3.976,4.470,4.898,4.000,3.583,3.728,4.496,4.197)
 
-ggplot(data = datos, aes(x = concurrencia, y = solsSegundo, fill = solicitudes)) +
+solicitudes.factor <- factor(solicitudes)
+concurrencia.factor <- factor(concurrencia)
+
+as.numeric(solicitudes.factor)
+as.character(concurrencia.factor)
+
+datos <- data.frame(solicitudes.factor, concurrencia.factor, solicitudesSegundo)
+
+ggplot(data = datos, aes(x = concurrencia.factor, y = solicitudesSegundo, fill = solicitudes.factor)) +
   geom_bar(stat = "identity", position = position_dodge(), colour = "black") + 
   coord_cartesian(ylim=c(0, 5)) + 
   scale_y_continuous(breaks=seq(0, 5, 0.5)) + 
--- graphics/data/tiempoEjecucion.R
+++ graphics/data/tiempoEjecucion.R
@@ -1,12 +1,19 @@
 #!/usr/bin/env Rscript
 library("ggplot2")
 
-datos <- data.frame(solicitudes = factor(c("30","30","30","50","50","50","100","100","100")),
-                    concurrencia = factor(c("25%","50%","75%","25%","50%","75%","25%","50%","75%"),
-                                          levels = c("25%","50%","75%")),
-                    segundos = c(8.823,7.552,6.827,10.215,12.506,13.959,26.966,22.850,24.473))
+solicitudes <- c(30,30,30,50,50,50,100,100,100)
+concurrencia <- c("25%","50%","75%","25%","50%","75%","25%","50%","75%")
+tiempo <- c(8.823,7.552,6.827,10.215,12.506,13.959,26.966,22.850,24.473)
 
-ggplot(data = datos, aes(x = concurrencia, y = segundos, fill = solicitudes)) +
+solicitudes.factor <- factor(solicitudes)
+concurrencia.factor <- factor(concurrencia)
+
+as.numeric(solicitudes.factor)
+as.character(concurrencia.factor)
+
+datos <- data.frame(solicitudes.factor, concurrencia.factor, tiempo)
+
+ggplot(data = datos, aes(x = concurrencia.factor, y = tiempo, fill = solicitudes.factor)) +
   geom_bar(stat = "identity", position = position_dodge(), colour = "black") + 
   coord_cartesian(ylim=c(0, 30)) + 
   scale_y_continuous(breaks=seq(0, 30, 2.5)) + 
